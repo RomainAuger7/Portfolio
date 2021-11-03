5be856d47986fddf503b8d8b53ab04d4ab4b8677
Romain <romain.auger.jd@gmail.com>
2021-11-03 12:13:48 +0100

    :sparkles: Introduce a little pokemon fight in the arena page

--- view/home.php
+++ view/home.php
@@ -3,15 +3,14 @@
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../assets/css/home.css"/>
+        <link rel="stylesheet" href="../assets/styles/home.css"/>
         <title>Portfolio | Romain Auger</title>
     </head>
     <body>
     <?php include 'components/header.php' ?>
-       
             <br/>
             <article>
-                <p>Welcome, this website is a tribute to an unique legend of our world, the one and only Romain Auger. Legend of his time and skilled warrior, he made a strong contribution during his lifetime to the growth and advancement of mankind.</p> 
+                <p><h3>Welcome, this website is a tribute to an unique legend of our world, the one and only Romain Auger. Legend of his time and skilled warrior, he made a strong contribution during his lifetime to the growth and advancement of mankind.</h3></p> 
                 <div class="portrait">
                     <h2>Portrait of our hero</h2>
                     <img class="gladiator" src="../assets/media/gladiator.jpg"/>
--- view/tribute.php
+++ view/tribute.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../assets/css/tribute.css"/>
+        <link rel="stylesheet" href="../assets/styles/tribute.css"/>
         <title>Portfolio | Romain Auger</title>
     </head>
     <body>
