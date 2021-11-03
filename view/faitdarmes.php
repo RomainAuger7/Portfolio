4d761e9fc5d6dc161463f2b97c86420f51e8a266
Romain <romain.auger.jd@gmail.com>
2021-11-03 09:49:39 +0100

    :tada: First commit, define structure and first site page

---
+++ view/faitdarmes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../assets/css/faitdarmes.css"/>
+        <title>Portfolio | Romain Auger</title>
+    </head>
+    <body>
+    
+        <article>
+            <div>
+                
+            </div>
+        </article>
+        
+    </body>
+</html> 
---
+++ view/hommage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../assets/css/hommage.css"/>
+        <title>Portfolio | Romain Auger</title>
+    </head>
+    <body>
+         
+        <section>
+            <br/>
+            <div id="triste">
+                <p>Rip in peace :'(</p>
+                <img class="rip" src="../assets/media/rip.jpg"/>
+            </div>
+            
+        </section>
+    </body>
+</html> 
