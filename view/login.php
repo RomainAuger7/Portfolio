758696f538ffe6d14d843d3092110ac50b709bf6
Romain <romain.auger.jd@gmail.com>
2021-11-03 16:14:43 +0100

    :art: Fix display issue on form

--- view/login.php
+++ view/login.php
@@ -8,37 +8,37 @@
     </head>
     <body>
     <?php include 'components/header.php' ?>
-       
-        <div class="field">
-        <label class="label">Username :</label>
-        <div class="control">
-            <input class="input" type="text" placeholder="Username..">
-        </div>
-        </div>
+       <form method="POST">
+            <div class="field">
+            <label class="label">Username :</label>
+            <div class="control">
+                <input class="input" type="text" placeholder="Username..">
+            </div>
+            </div>
 
-        <div class="field">
-        <label class="label">Password :</label>
-        <div class="control has-icons-left has-icons-right">
-            <input class="input is-success" type="password">
-            <span class="icon is-small is-left">
-            <i class="fas fa-user"></i>
-            </span>
-            <span class="icon is-small is-right">
-            <i class="fas fa-check"></i>
-            </span>
-        </div>
-        </div>
+            <div class="field">
+            <label class="label">Password :</label>
+            <div class="control has-icons-left has-icons-right">
+                <input class="input is-success" type="password">
+                <span class="icon is-small is-left">
+                <i class="fas fa-user"></i>
+                </span>
+                <span class="icon is-small is-right">
+                <i class="fas fa-check"></i>
+                </span>
+            </div>
+            </div>
 
 
-        <div class="field is-grouped">
-        <div class="control">
-            <button class="button is-link">Submit</button>
-        </div>
-        <div class="control">
-            <button class="button is-link is-light">Cancel</button>
-        </div>
-        </div>
-
+            <div class="field is-grouped">
+            <div class="control">
+                <button class="button is-link">Submit</button>
+            </div>
+            <div class="control">
+                <button class="button is-link is-light">Cancel</button>
+            </div>
+            </div>
+       </form>
     <?php include 'components/footer.php' ?>    
     </body>
 </html> 
